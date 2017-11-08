763b9123bb78b8d60aeca92c40655b9f1d0cbc42
David Blacher <cyberspeck@users.noreply.github.com>
2017-11-08 11:42:36 +0100

    added next Angabbe

--- 02-Ue-2017-10-11/UEProgramme/d3.py
+++
@@ -1,7 +0,0 @@
-# Iteration durch mehrere Listen mit zip
-l=[1,2,3,'wert',3.14,(0,1)]
-l1=[7,2,3,'wert',3.14,(0,1),{'edv':5,'mathe':5}]
-l2=[9,2,3,'wert',3.14,(0,1),{'edv':5,'mathe':5}]
-
-for a, b, c in zip(l,l1,l2):
-   print a,b,c
--- 02-Ue-2017-10-11/UEProgramme/test_fig0.py
+++
@@ -1,3 +0,0 @@
-import fig0
-
-print fig0.Rechteck(.7,5)
