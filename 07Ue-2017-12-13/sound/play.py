3e6d0326bbfaeac4b7eb0bca64795162012a0745
David Blacher <cyberspeck@users.noreply.github.com>
2018-01-17 11:03:04 +0100

    chris hat seine Magic dem Ton eingehaucht

--- 07Ue-2017-12-13/sound/play.py
+++ 07Ue-2017-12-13/sound/play.py
@@ -14,6 +14,9 @@ import time
 import getopt
 import alsaaudio
 import numpy as np
+import matplotlib.pyplot as plt
+#from scipy.io import wavfile as wav
+#from scipy.fftpack import fft
 from math import *
 from getopt import *
 from random import *
@@ -29,8 +32,8 @@ def make_ramp(f0=30.,df=2000., ampl=100000., rate=22050,length=5):
     wav += chr(f & 0x00FF) + chr((f & 0xFF00) >> 8)
   return wav
 # -------------------------------------
-def make_sin(f0=1000.,ampl=100000,rate=22050,length=5.):
-  a = 2. * pi * f0/rate              
+def make_sin(f0=100.,ampl=500000.,rate=22050,length=5.):
+  a = 2. * pi * f0/rate             
   n = int(rate * length)
   wav=''
   for i in range(0, n):
@@ -49,13 +52,45 @@ def make_noise(ampl=30000,rate=22050,length=5.):
   return wav
 # ------------------------------------------
 def make_Rechteck(ampl=30000,rate=22050,length=5.):
-
+  print ("Rechteck")
+  liste=[]
+  N=[]
+  f=int(0)
   n = int(rate * length)
-  df_step = df / rate
+  #for i in range(n):
+   # N.append(i)		
+  #N=np.arrange(n)
   wav=''
   for i in range(0, n):
-    f = int(30000)
+    if i%10 < 5:
+      f=1500
+    else:
+      f=-1500
     wav += chr(f & 0x00FF) + chr((f & 0xFF00) >> 8)
+    liste.append(f)
+  #plt.plot(N, liste)
+  #plt.show()
+  return wav
+# -------------------------------------
+def make_Dreieck(f0=1000.,ampl=1000,rate=22050,length=5.):
+  print('Dreieck')
+  liste=[]
+  N=[]            
+  n = int(rate * length)
+  for i in range(n):
+    N.append(i)
+  wav=''
+  delta_t=200
+  for i in range(0, n):
+    if i%delta_t < (delta_t/2):
+      f = int((i%(delta_t/2))*ampl)
+    else:
+      f=int(((delta_t/2)-i%(delta_t/2))*ampl)
+    print f
+    liste.append(f)
+    wav += chr(f & 0x00FF) + chr((f & 0xFF00) >> 8)
+  plt.plot(N, liste)
+  plt.show()
   return wav
 # ------------------------------------------
 
@@ -80,7 +115,8 @@ if __name__ == '__main__':
   type_d={"sin": {"func": make_sin, "args":{"f0":freq,"ampl":ampl,"rate":22050,"length":tr}},
          "ramp": {"func": make_ramp,"args":{"f0":freq,"df":df,"ampl":ampl,"rate":22050,"length":tr}},
          "noise": {"func": make_noise,"args":{"ampl":ampl,"rate":22050,"length":tr}},
-         "Rechteck": {"func": make_Rechteck,"args":{"ampl":ampl,"rate":22050,"length":tr}} }
+         "Rechteck": {"func": make_Rechteck,"args":{"ampl":ampl,"rate":22050,"length":tr}},
+		 "Dreieck": {"func": make_Dreieck,"args":{"ampl":ampl,"rate":22050,"length":tr}}	 }
   wave=type_d[sound]["func"](**type_d[sound]["args"])
 
   # Open the device in playback mode. 
@@ -98,8 +134,26 @@ if __name__ == '__main__':
 
 
 
+"""
+  for i in range(n/4, n/2):
+    f = int(ampl*sin(150*i))
+    wav += chr(f & 0x00FF) + chr((f & 0xFF00) >> 8)
+  for i in range(n/2, 3*n/4):
+    f = int(10000+ampl*sin(150*i))
+    wav += chr(f & 0x00FF) + chr((f & 0xFF00) >> 8)
+  for i in range(3*n/4, n):
+    f = int(ampl*sin(150*i))
+    wav += chr(f & 0x00FF) + chr((f & 0xFF00) >> 8)
+"""
+"""
+    for j in range(1,7,2):
 
-
+      f=f+int(ampl*sin(j*100*i)/j)
+      #print i, j, f, sin(j*1000*i)
+    #f = int((ampl*sin(150*i)
+    #print ('Gesamt' ,f, j)
+    liste.append(f)
+"""
 
 
 
--- 07Ue-2017-12-13/sound/record.py
+++ 07Ue-2017-12-13/sound/record.py
@@ -4,7 +4,7 @@
      -o filename: Write output to filename.raw and filename.dat (default: record16)
   RESULT:
    Records t seconds from your default sound card.
-   With a sample rate of 22050 Hz in format S16LE (16 bit int little endian)
+   With a sample rate of 22050 Hz in format S16LE (16 bit int little endian)  
 """
 import sys
 import time
@@ -13,6 +13,8 @@ import alsaaudio
 import numpy as np
 from math import *
 from getopt import *
+import matplotlib.pyplot as plt
+#from scipy.io import wavfile as wav	
 
 if __name__ == '__main__':
   opt=getopt(sys.argv[1:], "t:o:h")
@@ -44,7 +46,7 @@ if __name__ == '__main__':
       iwav += data
     time.sleep(.00005)
 
-
+  
   fiwav = open(out_filename+".raw", "w")
   fiwav.write(iwav)
   fiwav.close()
@@ -56,22 +58,53 @@ if __name__ == '__main__':
     lb = ord(iwav[2*i])
     hb = ord(iwav[2*i+1])
     f = (hb << 8) | lb
-    rec_arr.append(f)
+    
     if hb > 127:
       f -= 65536
+    rec_arr.append(f)
     print >> fidat, f
-
+  
   fidat.close()
 
+
+  Fs = 22050  # sampling rate
+  Ts = 1.0/Fs
+  t = np.arange(0,1,Ts) # time vector
+
+  ff = 5;   # frequency of the signal
+ 
+
+  n = len(rec_arr) # length of the signal
+  k = np.arange(n)
+  T = n/Fs
+  frq = k/T # two sides frequency range
+  frq = frq[range(n/2)] # one side frequency range
+
+  #Y = np.fft.fft(y)/n # fft computing and normalization
+  #Y = Y[range(n/2)]
+
+
+  #plt.plot(rec_arr)
   #fidat = open(out_filename+".dat", "r")
-  sp = np.fft.fft(rec_arr)
- # for i, j in enumerate(rec_arr):
-	#print i,j
-  print("\n")
-  for i, j in enumerate(sp):
-	print i,j
-
-
+  sp = np.fft.fft(rec_arr)/len(rec_arr)
+  sp=sp[range(len(rec_arr)/2)]
+  #plt.plot(np.abs(sp))
+  
+  fig, ax = plt.subplots(2, 1)
+  ax[0].plot(rec_arr)
+  ax[0].set_xlabel('Time')
+  ax[0].set_ylabel('Amplitude')
+  ax[1].plot(frq,abs(sp),'r') # plotting the spectrum
+  ax[1].set_xlabel('Freq (Hz)')
+  ax[1].set_ylabel('|Y(freq)|')
+  plt.show()
+"""
+  rate, data = wav.read('record16.raw')
+  fft_out = fft(data)
+  #matplotlib inline
+  plt.plot(data, np.abs(fft_out))
+  plt.show() 
+"""  
 
 
 
